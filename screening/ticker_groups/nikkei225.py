bd6fe8dcb61e07ed2d15fa55d65a75e545a99449
Adam Smith <adamsmith4494@gmail.com>
2026-07-30 16:05:42 +0900

    Reorder ETFs

--- screening/ticker_groups/nikkei225.py
+++ screening/ticker_groups/nikkei225.py
@@ -1,12 +1,12 @@
 NIKKEI225 = [
-  "1330", # Amova Exchange Traded Index Fund Nikkei 225
-  "1346", # MAXIS NIKKEI225 ETF
-  "1578", # Listed Index Fund Nikkei 225 (Mini)
-  "1369", # One ETF Nikkei225
-  "1397", # SMDAM NIKKEI225 ETF
   "1320", # iFreeETF Nikkei225 (Yearly Dividend Type)
   "1321", # NEXT FUNDS Nikkei 225 Exchange Traded Fund
   "1329", # iShares Core Nikkei 225 ETF
+  "1330", # Amova Exchange Traded Index Fund Nikkei 225
+  "1346", # MAXIS NIKKEI225 ETF
+  "1369", # One ETF Nikkei225
+  "1397", # SMDAM NIKKEI225 ETF
+  "1578", # Listed Index Fund Nikkei 225 (Mini)
   "2525", # NZAM ETF Nikkei 225
   "2624", # iFreeETF-Nikkei225(Quarterly Dividend Type)
   "473A", # Nissay ETF Nikkei 225 Index
